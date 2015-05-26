2d7e6e21e25a5c9c9e0804af4376a5a9d019f190
Alexander Ewetumo <trinoxf@gmail.com>
2015-05-26 01:38:49 +0100

    seperating package src

---
+++ sockets.go
@@ -0,0 +1,237 @@
+package flux
+
+import (
+	"log"
+	"runtime"
+	"sync"
+)
+
+//SocketInterface defines member function rules
+type SocketInterface interface {
+	Emit(interface{})
+	Stream(chan interface{})
+	addListenerIndex(func(interface{})) int
+	removeListenerIndex(int)
+	Subscribe(func(interface{}, *Sub)) *Sub
+	Size() int
+	PoolSize() int
+	Close()
+	ClearListeners()
+	ListenerSize() int
+}
+
+//Pipe represent the interface that defines the behaviour of Push and any other
+//type build on top of the SocketInterface type
+type Pipe interface {
+	SocketInterface
+	PushStream()
+	Wait()
+}
+
+//Sub provides a nice clean subscriber connection for socket
+type Sub struct {
+	socket  SocketInterface
+	pin     int
+	fnx     func(interface{}, *Sub)
+	removed bool
+}
+
+//Close disconnects the subscription
+func (s *Sub) Close() {
+	if s.socket == nil {
+		return
+	}
+	s.socket = nil
+	s.removed = true
+	// s.socket.removeListenerIndex(s.pin)
+	s.pin = 0
+}
+
+//NewSub returns a new subscriber
+func NewSub(sock SocketInterface, fn func(interface{}, *Sub)) *Sub {
+	sd := &Sub{sock, -1, fn, false}
+	sd.pin = sock.addListenerIndex(func(v interface{}) {
+		if !sd.removed {
+			sd.fnx(v, sd)
+		}
+	})
+	return sd
+}
+
+//Socket is the base structure for all data flow communication
+type Socket struct {
+	channel    chan interface{}
+	closer     chan struct{}
+	listeners  *SingleStack
+	bufferSize int
+	bufferup   bool
+	when       *sync.Once
+}
+
+//PoolSize returns the size of data in the channel
+func (s *Socket) PoolSize() int {
+	return s.bufferSize
+}
+
+//ClearListeners clears the sockets listener list
+func (s *Socket) ClearListeners() {
+	s.listeners.Clear()
+}
+
+//Close closes the socket internal channel and clears its listener list
+func (s *Socket) Close() {
+	close(s.channel)
+	<-s.closer
+	// s.listeners.lock.Lock()
+	s.ClearListeners()
+	// s.listeners.lock.Unlock()
+}
+
+//Subscribe returns a subscriber
+func (s *Socket) Subscribe(fx func(interface{}, *Sub)) *Sub {
+	// if s.bufferup {
+	// 	close(s.begin)
+	// }
+	return NewSub(s, fx)
+}
+
+//ListenerSize returns the size of listeners
+func (s *Socket) ListenerSize() int {
+	return s.listeners.Size()
+}
+
+//Size returns the size of data in the channel
+func (s *Socket) Size() int {
+	return len(s.channel)
+}
+
+//Emit adds a new data into the channel
+func (s *Socket) Emit(b interface{}) {
+	s.channel <- b
+}
+
+//RemoveListenerIndex adds a function into the socket queue
+func (s *Socket) removeListenerIndex(ind int) {
+	s.listeners.Delete(ind)
+}
+
+//AddListenerIndex adds a function into the socket queue
+func (s *Socket) addListenerIndex(f func(interface{})) int {
+	return s.listeners.Add(f)
+}
+
+//Stream provides a means of piping the data within a channel into
+//the sockets channel,ensure to close the chan passed as Stream uses
+//range to iterate the channel
+func (s *Socket) Stream(data chan interface{}) {
+	for k := range data {
+		s.Emit(k)
+	}
+}
+
+//NewSocket returns a new socket instance
+func NewSocket(size int, buf bool) *Socket {
+	li := NewSingleStack()
+	return &Socket{
+		make(chan interface{}, size),
+		make(chan struct{}),
+		li,
+		size,
+		buf,
+		new(sync.Once),
+	}
+}
+
+//Push creates a push-like socket
+type Push struct {
+	*Socket
+	pin  *Sub
+	wait *sync.WaitGroup
+	// closer chan struct{}
+}
+
+//Emit adds a new data into the channel
+func (p *Push) Emit(b interface{}) {
+	if !p.bufferup {
+		if p.Socket.ListenerSize() <= 0 {
+			return
+		}
+	}
+	log.Printf("Emitting: size: %v data:%+v", p.Socket.ListenerSize(), b)
+	p.Socket.Emit(b)
+}
+
+//Close clears the listerns lists and stops listen to parent if existing
+func (p *Push) Close() {
+	if p.pin != nil {
+		p.pin.Close()
+	}
+	p.Socket.Close()
+}
+
+//Wait caues the socket to wait till its done
+func (p *Push) Wait() {
+	p.wait.Wait()
+}
+
+//PushStream uses the range iterator over the terminal
+func (p *Push) PushStream() {
+	p.wait.Add(1)
+	go func() {
+		// <-p.begin
+		for dx := range p.Socket.channel {
+			p.listeners.Each(dx)
+			runtime.Gosched()
+		}
+		p.wait.Done()
+		p.when.Do(func() {
+			close(p.closer)
+		})
+	}()
+}
+
+//PushSocket returns the socket wrapped up in the Push struct
+func PushSocket(buff int) *Push {
+	ps := &Push{
+		NewSocket(buff, false),
+		nil,
+		new(sync.WaitGroup),
+	}
+	// close(ps.begin)
+	ps.PushStream()
+	return ps
+}
+
+//PushSocketWith returns the socket wrapped up in the Push struct
+func PushSocketWith(sock SocketInterface) *Push {
+	su := NewSocket(sock.PoolSize(), false)
+	ps := &Push{
+		su,
+		sock.Subscribe(func(v interface{}, _ *Sub) {
+			su.Emit(v)
+		}),
+		new(sync.WaitGroup),
+	}
+
+	// close(ps.begin)
+	ps.PushStream()
+	return ps
+}
+
+//DoPushSocket creates a pull socket based on a condition
+func DoPushSocket(sock SocketInterface, fn func(f interface{}, sock SocketInterface)) *Push {
+	su := NewSocket(sock.PoolSize(), false)
+	ps := &Push{
+		su,
+		nil,
+		new(sync.WaitGroup),
+	}
+
+	// close(ps.begin)
+	ps.pin = sock.Subscribe(func(v interface{}, _ *Sub) {
+		fn(v, ps)
+	})
+
+	ps.PushStream()
+	return ps
+}
---
+++ waiter.go
@@ -0,0 +1,175 @@
+package flux
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+//WaitInterface defines the flux.Wait interface method definitions
+type WaitInterface interface {
+	Add()
+	Done()
+	Count() int
+	Flush()
+	Then() ActionInterface
+}
+
+//SwitchInterface defines a flux.Switch interface method definition
+type SwitchInterface interface {
+	Switch()
+	IsOn() bool
+	WhenOn() ActionInterface
+	WhenOff() ActionInterface
+}
+
+type baseWait struct {
+	action ActionInterface
+}
+
+//Then returns an ActionInterface which gets fullfilled when this wait
+//counter reaches zero
+func (w *baseWait) Then() ActionInterface {
+	return w.action.Wrap()
+}
+
+func newBaseWait() *baseWait {
+	return &baseWait{NewAction()}
+}
+
+//TimeWait defines a time lock waiter
+type TimeWait struct {
+	*baseWait
+	closer chan struct{}
+	hits   int64
+	ms     time.Duration
+	doonce *sync.Once
+}
+
+//NewTimeWait returns a new timer wait locker
+func NewTimeWait(max int, duration time.Duration) *TimeWait {
+
+	if max <= 0 {
+		max = 1
+	}
+
+	tm := &TimeWait{
+		newBaseWait(),
+		make(chan struct{}),
+		int64(max),
+		duration,
+		new(sync.Once),
+	}
+
+	go tm.handle()
+
+	return tm
+}
+
+//handle effects the necessary time process for checking and reducing the
+//time checker for each duration of time,till the Waiter is done
+func (w *TimeWait) handle() {
+iotick:
+	for {
+		select {
+		case <-time.After(w.ms):
+			w.Done()
+		case <-w.closer:
+			break iotick
+		}
+	}
+}
+
+//Flush drops the lock count and forces immediate unlocking of the wait
+func (w *TimeWait) Flush() {
+	w.doonce.Do(func() {
+		close(w.closer)
+		w.action.Fullfill(0)
+		w.hits = int64(0)
+	})
+}
+
+//Count returns the total left count to completed before unlock
+func (w *TimeWait) Count() int {
+	return int(w.hits)
+}
+
+//Add increments the lock state to the lock counter unless its already unlocked
+func (w *TimeWait) Add() {
+	if w.Count() < 0 {
+		return
+	}
+
+	atomic.AddInt64(&w.hits, 1)
+}
+
+//Done decrements the totalcount of this waitlocker by 1 until its below zero
+//and fullfills with the 0 value
+func (w *TimeWait) Done() {
+	hits := atomic.LoadInt64(&w.hits)
+
+	if hits < 0 {
+		return
+	}
+
+	newhit := atomic.AddInt64(&w.hits, -1)
+
+	if int(newhit) < 0 {
+		w.Flush()
+	}
+}
+
+//Wait implements the WiatInterface for creating a wait lock which
+//waits until the lock lockcount is finished then executes a action
+//can only be used once, that is ,once the wait counter is -1,you cant add
+//to it anymore
+type Wait struct {
+	*baseWait
+	totalCount int64
+}
+
+//NewWait returns a new Wait instance for the WaitInterface
+func NewWait() WaitInterface {
+	return &Wait{newBaseWait(), int64(0)}
+}
+
+//Flush drops the lock count and forces immediate unlocking of the wait
+func (w *Wait) Flush() {
+	curr := int(atomic.LoadInt64(&w.totalCount))
+	for curr >= 0 {
+		w.Done()
+		curr--
+	}
+}
+
+//Count returns the total left count to completed before unlock
+func (w *Wait) Count() int {
+	return int(atomic.LoadInt64(&w.totalCount))
+}
+
+//Add increments the lock state to the lock counter unless its already unlocked
+func (w *Wait) Add() {
+	curr := atomic.LoadInt64(&w.totalCount)
+
+	if curr < 0 {
+		return
+	}
+
+	atomic.AddInt64(&w.totalCount, 1)
+}
+
+//Done decrements the totalcount of this waitlocker by 1 until its below zero
+//and fullfills with the 0 value
+func (w *Wait) Done() {
+	curr := atomic.LoadInt64(&w.totalCount)
+
+	if curr < 0 {
+		return
+	}
+
+	nc := atomic.AddInt64(&w.totalCount, -1)
+
+	if int(nc) < 0 {
+		w.action.Fullfill(0)
+	}
+}
