d5e2471c3e48d0d9ae13b666124ef0991cfb321c
Ewetumo Alexander <trinoxf@gmail.com>
2015-08-14 23:36:57 +0100

    update library

--- flux.go
+++ flux.go
@@ -9,8 +9,108 @@ import (
 
 var log = logging.MustGetLogger("flux")
 
+//Collector defines a typ of map string
+type Collector map[string]interface{}
+
 //Eachfunc defines the type of the Mappable.Each rule
 type Eachfunc func(interface{}, interface{}, func())
+
+//StringEachfunc defines the type of the Mappable.Each rule
+type StringEachfunc func(interface{}, string, func())
+
+//StringMappable defines member function rules for securemap
+type StringMappable interface {
+	Clear()
+	HasMatch(k string, v interface{}) bool
+	Each(f StringEachfunc)
+	Keys() []string
+	Copy(map[string]interface{})
+	Has(string) bool
+	Get(string) interface{}
+	Remove(string)
+	Set(k string, v interface{})
+	Clone() StringMappable
+}
+
+//NewCollector returns a new collector instance
+func NewCollector() Collector {
+	return make(Collector)
+}
+
+//Clone makes a new clone of this collector
+func (c Collector) Clone() StringMappable {
+	col := make(Collector)
+	col.Copy(c)
+	return col
+}
+
+//Remove deletes a key:value pair
+func (c Collector) Remove(k string) {
+	if c.Has(k) {
+		delete(c, k)
+	}
+}
+
+//Set puts a specific key:value into the collector
+func (c Collector) Set(k string, v interface{}) {
+	c[k] = v
+}
+
+//Copy copies the map into the collector
+func (c Collector) Copy(m map[string]interface{}) {
+	for v, k := range m {
+		c.Set(v, k)
+	}
+}
+
+//Each iterates through all items in the collector
+func (c Collector) Each(fx StringEachfunc) {
+	var state bool
+	for k, v := range c {
+		if state {
+			break
+		}
+
+		fx(v, k, func() {
+			state = true
+		})
+	}
+}
+
+//Keys return the keys of the Collector
+func (c Collector) Keys() []string {
+	var keys []string
+	c.Each(func(_ interface{}, k string, _ func()) {
+		keys = append(keys, k)
+	})
+	return keys
+}
+
+//Get returns the value with the key
+func (c Collector) Get(k string) interface{} {
+	return c[k]
+}
+
+//Has returns if a key exists
+func (c Collector) Has(k string) bool {
+	_, ok := c[k]
+	return ok
+}
+
+//HasMatch checks if key and value exists and are matching
+func (c Collector) HasMatch(k string, v interface{}) bool {
+	if c.Has(k) {
+		return c.Get(k) == v
+	}
+	return false
+}
+
+//Clear clears the collector
+func (c Collector) Clear() {
+	for k := range c {
+		delete(c, k)
+	}
+}
 
 //Mappable defines member function rules for securemap
 type Mappable interface {
