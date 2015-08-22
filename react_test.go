5d8bbed6abf906f637dc4f04e99c6bf201d0e97c
Ewetumo Alexander <trinoxf@gmail.com>
2015-08-22 20:32:21 +0100

    adding base test

--- react_test.go
+++ react_test.go
@@ -5,13 +5,24 @@ import (
 	"testing"
 )
 
-func TestReactiveStack(t *testing.T) {
+//TestBasicReaction provides a test case for a simple two-way reaction i.e receive and react infinitely
+//to data being sent into a pipe until that pipe is closed
+func TestBasicReaction(t *testing.T) {
 
 	ws := new(sync.WaitGroup)
-	gen := ReactIdentity()
+
+	//ReactIdentity returns reactor that simply pipes what input it gets to its output if it has a connection
+
+	//collect is the root reactor
+	collect := ReactIdentity()
 
 	ws.Add(2)
-	mn := gen.React(func(k ReactiveStacks) {
+
+	//.React() is the simple approach of branching out from a root reactor,you receive the new reactor its self
+	//but are able to get the parent reactor using the .Feed() function and collect the data from its Out() pipe
+	//all reactor provide a .Close() channel that lets you listen in for when to stop operations,this way you get to decide
+	//if you wish to stop the new reactor along
+	mn := collect.React(func(k ReactiveStacks) {
 		func() {
 		mloop:
 			for {
@@ -20,7 +31,7 @@ func TestReactiveStack(t *testing.T) {
 					ws.Done()
 				case <-k.Feed().Out():
 					ws.Done()
-				case <-gen.Closed():
+				case <-k.Feed().Closed():
 					k.End()
 					break mloop
 				}
@@ -28,10 +39,13 @@ func TestReactiveStack(t *testing.T) {
 		}()
 	})
 
-	gen.In() <- 2
-	mn.In() <- 40
+	/*we can emit/send data for reaction using the in-channel returned bythe In() function*/
+	collect.In() <- 2 //sends data into the root reactor
+	mn.In() <- 40     //sends data only into the current reactor
 
 	ws.Wait()
-	gen.End()
+
+	//Reactors are closed by calling the .End() function
+	collect.End()
 
 }
