4c43d28a63eb7a5392e0eccf0558e6611b5f2b20
Ewetumo Alexander <trinoxf@gmail.com>
2015-08-25 15:04:45 +0100

    removed print

--- reactstack_test.go
+++ reactstack_test.go
@@ -2,7 +2,6 @@ package flux
 
 import (
 	"errors"
-	"log"
 	"sync"
 	"testing"
 )
@@ -16,8 +15,6 @@ func TestChannelConnect(t *testing.T) {
 	feed.SendClose(404)
 
 	do := <-stream.Signals()
-
-	log.Printf("do:", do)
 
 	if do != 20 {
 		t.Fatalf("Inccorect value received %d expected %d", do, 20)
